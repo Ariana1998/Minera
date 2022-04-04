b33877408a5a6a52579f34e248bddceced954709
Rafael-Carballo <74878734+Rafael-Carballo@users.noreply.github.com>
2022-04-04 06:39:36 -0700

    prueba de concepto del proceso login
    prueba de concepto del proceso de login

--- Mineria/src/DAO/AccesoI.java
+++ Mineria/src/DAO/AccesoI.java
@@ -3,19 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mineria;
+package DAO;
+
+import objetoNegocio.Usuario;
 
 /**
  *
  * @author darkl
  */
-public class Mineria {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public interface AccesoI {
+  
+    public boolean accesar(String usuario, String contraseña);
+    public boolean registrar(Usuario usuario);
+    
     
 }
